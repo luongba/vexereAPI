dde1ce6e217e519cede32c7e9260ab4470f20ce4
luongba <a2k10lnc@gmail.com>
2022-08-05 15:53:20 +0700

    file upload

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const { register, login } = require("../controllers/user.controller");
 const userRouter = express.Router();
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/avatars/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "_" + file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+userRouter.post("/profile", upload.single("avatar"), (req, res) => {
+  res.send("uploads");
+});
 
 module.exports = {
-    userRouter
-}
+  userRouter,
+};
